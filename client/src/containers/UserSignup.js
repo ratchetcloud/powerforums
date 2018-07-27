f45a09e1b018102b8619e6f8a1e693405135949c
Jihye Jegal <seeker.jihye@gmail.com>
2018-07-27 16:03:06 -0700

    [CHANGE] indentation

--- client/src/containers/UserSignup.js
+++ client/src/containers/UserSignup.js
@@ -6,38 +6,38 @@ import { userSignupEnded } from '../actions/userActions';
 
 
 class UserSignup extends Component {
-	constructor(props) {
-		super(props);
-	}
+    constructor(props) {
+        super(props);
+    }
 
     componentWillUnmount(){
         this.props.signupEnd() 
     }
 
-	render(){
-		const { loading, success } = this.props;
+    render(){
+        const { loading, success } = this.props;
         if (loading === false && success === false) {
             // If there user is not authenticated, show LoginForm
             return (
-            	<div>
-            		<SignupUserForm />
-            	</div>
+                <div>
+                    <SignupUserForm />
+                </div>
             )
 
         } else if (loading === true && success === false) {
             // If authentication request is pending, return nothing.
             return <div>Loading..</div>
         } else {
-        	// signup success
-        	return (
-        		<div>signup Success.<br />
-	        		<button>
-	                        <NavLink to="/">Go home</NavLink>
-	                </button>
+            // signup success
+            return (
+                <div>signup Success.<br />
+                    <button>
+                        <NavLink to="/">Go home</NavLink>
+                    </button>
                 </div>
-        	)
+            )
         }
-	}
+    }
 }
 
 const mapStateToProps = state => ({
@@ -46,7 +46,7 @@ const mapStateToProps = state => ({
 });
 
 const mapDispatchToProps = dispatch => ({
-	signupEnd: () => {
+    signupEnd: () => {
        return dispatch(userSignupEnded())
     }
 });
