a53d21d46e8736059b9133d71fed65d23abd2eea
Prev <prevdev@gmail.com>
2018-08-08 11:26:43 -0700

    Fix login bug (Error message is not shown)

--- client/src/containers/Login/actions.js
+++ client/src/containers/Login/actions.js
@@ -4,26 +4,25 @@ export const meFromToken = (token) => (dispatch, getState, client) => {
     // If there is data on client sessionStorage, use that data
     // and dispatch userLoginFulfilled as if login is success
     let decoded = jwt.decode(token, {complete: true});
-    dispatch(userLoginFulfilled(token, decoded.payload))
+    dispatch(userLoginFulfilled(token, decoded.payload));
 };
 
 export const login = (username, password) => (dispatch, getState, client) => {
     // We want to handle an Async action, dispatch a "Loading" action.
     dispatch(loginPending());
 
-    client.login(username, password).then(response => {
-        // When API call is successful.
+    client.login(username, password)
+        .then(response => {
+            // Save token on client so that login state remains after refresh
+            sessionStorage.setItem('jwtToken', response.token);
 
-        // Save token on client so that login state remains after refresh
-        sessionStorage.setItem('jwtToken', response.token);
+            // Dispatch a fulfilled action.
+            dispatch(userLoginFulfilled(response.token, response.currentUser));
 
-        // Dispatch a fulfilled action.
-        dispatch(userLoginFulfilled(response.token, response.currentUser))
-
-    }).catch(response => {
-        // When an error (network or applicative) occurs, dispatch a rejected action.
-        dispatch(loginRejected(response))
-    })
+        })
+        .catch(error => {
+            dispatch(loginRejected(error.response.data));
+        })
 };
 
 const loginPending = () => ({
@@ -41,7 +40,7 @@ const userLoginFulfilled = (token, currentUser) => (dispatch, getState, client) 
             token: token,
             currentUser: currentUser
         }
-    })
+    });
 };
 
 const loginRejected = data => ({
@@ -50,12 +49,12 @@ const loginRejected = data => ({
 });
 
 export const logout = () => (dispatch, getState, client) => {
-    dispatch(logoutPending())
+    dispatch(logoutPending());
 
     sessionStorage.removeItem('jwtToken');
     client.setAuthorizationToken(false);
 
-    dispatch(logoutFulfilled())
+    dispatch(logoutFulfilled());
 };
 
 const logoutPending = () => ({
--- client/src/containers/Login/loginForm.js
+++ client/src/containers/Login/loginForm.js
@@ -1,8 +1,6 @@
-import React from 'react'
-import { Field, reduxForm } from 'redux-form'
-import { connect } from "react-redux";
-import * as userActions from './actions';
-import './loginForm.css'
+import React from 'react';
+import { Field, reduxForm } from 'redux-form';
+import './loginForm.css';
 
 class LoginForm extends React.Component {
     constructor(props) {
@@ -15,12 +13,13 @@ class LoginForm extends React.Component {
     }
 
     render() {
-        const { errMessage, handleSubmit } = this.props;
+        const { loginError, handleSubmit } = this.props;
         return (
             <div className="login container">
                 <div className="login-box">
                     <h2>Login</h2>
-                    <div style={{'color': 'red'}}>{errMessage}</div>
+                    {loginError && <div className="alert alert-danger" role="alert">{loginError}</div>}
+
                     <form onSubmit={handleSubmit(this.handleFormSubmit)}>
                         <div className="form-group mb-1">
                             <Field name="email"
@@ -46,15 +45,4 @@ class LoginForm extends React.Component {
     }
 }
 
-const mapStateToProps = (state) => ({
-});
-
-const mapDispatchToProps = (dispatch) => ({
-    login: (username, password) => {
-        dispatch(userActions.login(username, password))
-    }
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(
-    reduxForm({form: 'loginForm'})(LoginForm)
-)
+export default reduxForm({form: 'loginForm'})(LoginForm);
